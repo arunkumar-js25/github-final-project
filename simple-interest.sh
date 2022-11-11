ecd1f40eecc34a1f21159010477fb95f397be2d8
Arun Kumar <arunkumar.js25@gmail.com>
2022-11-11 21:26:50 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Arun Kumar J
    # Additional Authors:
-   # <your GitHub username>
+   # arunkumar-js25
 
    # Input:
    # p, principal amount
